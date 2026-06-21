c2e540fdc865dfe0db676e91f99570a7ea79de3c
David <davidsibanda634@gmail.com>
2026-06-21 20:55:30 +0530

    fix login to include isAdmin field

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,19 @@
-import User from "../models/User.js";
-import bcrypt from "bcryptjs";
-import generateToken from "../utils/generateToken.js";
-import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/User.js"
+import bcrypt from "bcryptjs"
+import generateToken from "../utils/generateToken.js"
+import asyncHandler from "../middleware/asyncHandler.js"
 
 // REGISTER
 export const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password, phone, location } = req.body;
+  const { name, email, password, phone, location } = req.body
 
-  const exists = await User.findOne({ email });
+  const exists = await User.findOne({ email })
   if (exists) {
-    res.status(400);
-    throw new Error("User already exists");
+    res.status(400)
+    throw new Error("User already exists")
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10)
 
   const user = await User.create({
     name,
@@ -21,33 +21,42 @@ export const registerUser = asyncHandler(async (req, res) => {
     password: hashedPassword,
     phone,
     location,
-  });
+  })
 
   res.status(201).json({
     _id: user._id,
     name: user.name,
     email: user.email,
     phone: user.phone,
+    isAdmin: user.isAdmin,
+    isBanned: user.isBanned,
     token: generateToken(user._id),
-  });
-});
+  })
+})
 
 // LOGIN
 export const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email })
 
   if (user && (await bcrypt.compare(password, user.password))) {
+    if (user.isBanned) {
+      res.status(403)
+      throw new Error("Your account has been suspended")
+    }
+
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       phone: user.phone,
+      isAdmin: user.isAdmin,
+      isBanned: user.isBanned,
       token: generateToken(user._id),
-    });
+    })
   } else {
-    res.status(401);
-    throw new Error("Invalid credentials");
+    res.status(401)
+    throw new Error("Invalid credentials")
   }
-});
+})
