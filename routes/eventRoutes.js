73cee84cc40559c77811f7a31e967652bc68f33c
David <davidsibanda634@gmail.com>
2026-08-05 21:06:42 +0530

    Step 8 - EventDetail and EditEvent updated with ticketing

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -12,7 +12,7 @@ import {
 } from "../controllers/eventController.js"
 import { protect } from "../middleware/authMiddleware.js"
 
-import { upload }  from "../middleware/uploadMiddleware.js"
+import upload  from "../middleware/uploadMiddleware.js"
 
 
 const router = express.Router()
